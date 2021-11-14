869487f730718d2c94fb0a75e14d43eb74ef46a0
Saplin136 <94320961+Saplin136@users.noreply.github.com>
2021-11-14 23:25:50 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 1
+            OperatingSystem os = System.Environment.OSVersion;
+            Console.WriteLine("Platform: {0}", os.Platform);
+            System.Console.WriteLine("The current date and time is " + System.DateTime.Now);
+            System.Console.ReadLine();
+
+            // Task 2
+            Console.WriteLine("Input value for variable x.");
+            Console.Write("x = ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Fx = " + (Math.Cos(1.5 * x) - Math.Exp(Math.Sin(x + (5/3))) + Math.Sqrt(x + (7/6))));
+
+        }
+    }
+}
